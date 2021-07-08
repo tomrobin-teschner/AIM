c186c01f8934e6fe1ffae927cd3db8638ac2579c
tomrobin-teschner <tomrobin.teschner@yahoo.de>
2021-07-08 07:46:26 +0100

    Added a coordinate Array class
    This class allows to wrap around a std::vector and
    expose custom functionality if required.

---
+++ src/groupDefinitions.hpp
@@ -0,0 +1,12 @@
+// This file is part of Artificial-based Incompressibile Methods 3D (AIM3D), a CFD solver for exact projection
+// methods based on hybrid Artificial compressibility and Pressure Projection methods.
+// (c) by Tom-Robin Teschner 2021. This file is distribuited under the MIT license.
+
+#pragma once
+
+/**
+ * \defgroup mesh A group for processing the computational mesh
+ *
+ * This group provides access to read a computation mesh from file and process it so that coordinates and
+ * boundary conditions become available.
+ */
